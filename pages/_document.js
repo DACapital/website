e3f7a946f19b6f1219012766f12658b67f495db6
João Mesquita <jcsmesquita@users.noreply.github.com>
2017-05-16 09:36:40 +0100

    Adding logo-name, setting background color, installing styled-components and adding compatibility for next.js (#3)

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  render () {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+          <style>{`
+            body {
+             background-color: #1C2541 
+            }
+          `}</style>
+          {styleTags}
+        </Head>
+        <body>
+          <div className='root'>
+            {main}
+          </div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
