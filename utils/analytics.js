da85b141ac534db10db851098af5d506f985eea8
Joao Carlos Mesquita <jcsmesquita@gmail.com>
2017-05-18 11:10:58 +0100

    Shame commit

--- utils/analytics.js
+++ utils/analytics.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga'
 // Initialize Google Analytics
 //
 export const initAnalytics = () => {
-  if (process.browser && process.env.production) {
+  if (process.browser && process.env.NODE_ENV === 'production') {
     console.log('GA init')
     ReactGA.initialize('UA-99334873-1')
   }
@@ -12,7 +12,7 @@ export const initAnalytics = () => {
 // Log Page Views
 //
 export const logPageView = () => {
-  if (process.browser && process.env.production) {
+  if (process.browser && process.env.NODE_ENV === 'production') {
     console.log('Pageview', window.location.pathname)
     ReactGA.set({ page: window.location.pathname })
     ReactGA.pageview(window.location.pathname)
