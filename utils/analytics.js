f4a356907e4ef3a19e56d0f80fc24c9632995533
Joao Carlos Mesquita <jcsmesquita@gmail.com>
2017-05-18 10:58:43 +0100

    Patched analytics to only run on production, using single quotes, adding some comments

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,14 +1,19 @@
 import ReactGA from 'react-ga'
-
+//
+// Initialize Google Analytics
+//
 export const initAnalytics = () => {
-  if (process.browser && process.production) {
+  if (process.browser && process.env.production) {
     console.log('GA init')
     ReactGA.initialize('UA-99334873-1')
   }
 }
+//
+// Log Page Views
+//
 export const logPageView = () => {
-  if (process.browser && process.production) {
-    console.log('Logging pageview', window.location.pathname)
+  if (process.browser && process.env.production) {
+    console.log('Pageview', window.location.pathname)
     ReactGA.set({ page: window.location.pathname })
     ReactGA.pageview(window.location.pathname)
   }
