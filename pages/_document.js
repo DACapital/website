85e5138693279e7658a6096166fd13cfc7866bda
Joao Carlos Mesquita <jcsmesquita@gmail.com>
2017-05-19 12:14:27 +0200

    Add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,15 @@ export default class MyDocument extends Document {
       <html lang='en'>
         <Head>
           <title>DACapital</title>
-          <meta name='theme-color' content='#1C2541' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
+          {/* Favicons, manifest and theme color */}
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png' />
+          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='manifest' href='/static/manifest.json' />
+          <meta name='theme-color' content='#1C2541' />
+          {/* Stylesheets */}
           <link rel='stylesheet' href='/static/tachyons.css' />
           <link rel='stylesheet' href='/static/animate.min.css' />
           <link rel='stylesheet' href='/static/stars.css' />
